783dcac0d2617b93a0acc6d005e5d2632954388b
ParJai <parthjain22@gmail.com>
2023-01-18 09:33:28 -0500

    Update main.py
    Created pigLatin function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Hello world!")
+
+def pigLatin(phrase):
+    out = []
+    for word in phrase.split(' '):
+        if word[0].lower() in 'aeiou':
+            out.append(word + 'yay')
+        else:
+            out.append(word[1:] + word[0] + 'ay')
+    return ' '.join(out)
+
+print(pigLatin('This is a sentence'.split()))
