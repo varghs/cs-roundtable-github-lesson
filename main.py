a2f7f144e1a54ed897a41947e17f79157999b9cd
varghs <52459831+varghs@users.noreply.github.com>
2023-01-18 09:37:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ def pigLatin(phrase):
             out.append(word[1:] + word[0] + 'ay')
     return ' '.join(out)
 
-print(pigLatin('This is a sentence'.split()))
 def alphabeticalSort(words):
     for i in range(len(words)):
         minWordIndex = i
         for j in range(i, len(words)):
-            if words[j] < minWord:
+            if words[j] < words[minWordIndex]:
                 minWordIndex = j
         tmp = words[i]
         words[i] = words[minWordIndex]
@@ -22,8 +21,8 @@ def alphabeticalSort(words):
         
 
 def main():
-    print("Enter a list of words below, separated by spaces.")
-    words = [pigLatin(i.lower()) for i in input.split()]
+    print("Enter some words.")
+    words = pigLatin(input()).split()
     alphabeticalSort(words)
     print("Sorted words.")
     print(words)
