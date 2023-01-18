35ad25a7653302735be2b3f55d4a4d13f76fcd7f
varghs <52459831+varghs@users.noreply.github.com>
2023-01-18 09:33:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+def alphabeticalSort(words):
+    for i in range(len(words)):
+        minWordIndex = i
+        for j in range(i, len(words)):
+            if words[j] < minWord:
+                minWordIndex = j
+        tmp = words[i]
+        words[i] = words[minWordIndex]
+        words[minWordIndex] = tmp
+        
+
+def main():
+    print("Enter a list of words below, separated by spaces.")
+    words = [pigLatin(i.lower()) for i in input.split()]
+    alphabeticalSort(words)
+    print("Sorted words.")
+    print(words)
+
+if __name__ == "__main__":
+    main()
